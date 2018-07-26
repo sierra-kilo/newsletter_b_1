81b76779cacf60709aadbae83e97e243be712cca
Sierra Kilo <sierrakilo@Sierras-MBP.DFMNET01>
2018-07-26 10:51:04 -0700

    added placeholder for subscribe link

--- src/client/components/Footer.js
+++ src/client/components/Footer.js
@@ -3,6 +3,7 @@ import React from 'react';
 const Footer = ({}) => (
   <div>
     {/* links go here */}
+    <span> Subscribe | </span>
     <span> Unsubscribe | </span>
     <span> Admin | </span>
   </div>
