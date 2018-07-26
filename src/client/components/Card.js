6605c1f253804cb1bffc0b7e3cfa7c9ebcb905aa
Sierra Kilo <sierrakilo@Sierras-MBP.webpass.net>
2018-07-25 22:27:02 -0700

    imported Header into Card

--- src/client/components/Card.js
+++ src/client/components/Card.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+import Header from './Header'
 
 class Card extends Component {
 
   render() {
     return (
       <div>
-        {/* <Header/> */}
+        <Header/>
         {/* add react router here */}
         <h1>React Router will go here</h1>
         {/* <Footer/> */}
