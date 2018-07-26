07f96da795148c8ef418fa5c6f4c4fb56de4964a
Sierra Kilo <sierrakilo@Sierras-MBP.webpass.net>
2018-07-25 19:48:00 -0700

    added heroku-ssl-redirect |||| soon i now realized

--- src/server/index.js
+++ src/server/index.js
@@ -1,8 +1,11 @@
+const sslRedirect = require('heroku-ssl-redirect');
 const express = require('express');
-const os = require('os');
-
 const app = express();
 
+
+// middleware for production
 app.use(express.static('dist'));
-app.get('/api/getUsername', (req, res) => res.send({ username: os.userInfo().username }));
+
+
+
 app.listen(8080, () => console.log('Listening on port 8080!'));
