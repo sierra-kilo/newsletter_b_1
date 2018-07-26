0920b7feef0ca2be77fe141229470e48afcad8ae
Sierra Kilo <sierrakilo@Sierras-MBP.DFMNET01>
2018-07-26 10:51:31 -0700

    created skeleton for Admin Page, Subscribe Page, and Unsubscribe Page

---
+++ src/client/components/SubscribePage.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class SubscribePage extends Component {
+
+  render() {
+    return (
+      <div></div>
+    );
+  }
+
+}
+
+export default SubscribePage;
---
+++ src/client/components/UnsubscribePage.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class UnsubscribePage extends Component {
+
+  render() {
+    return (
+      <div></div>
+    );
+  }
+
+}
+
+export default UnsubscribePage;
