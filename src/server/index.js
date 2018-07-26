358b482ee3fafb3689de8cf44e867eaffc273cdb
Sierra Kilo <sierrakilo@Sierras-MBP.webpass.net>
2018-07-25 21:31:41 -0700

    server set up middlewares for data parsing

--- src/server/index.js
+++ src/server/index.js
@@ -2,9 +2,14 @@ const sslRedirect = require('heroku-ssl-redirect');
 const express = require('express');
 const app = express();
 
-
 // middleware for production
 app.use(express.static('dist'));
+
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
 
 
