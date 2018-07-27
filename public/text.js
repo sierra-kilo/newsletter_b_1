2674b4a79e05638b7a8ee915a1cf841912378e66
Sierra Kilo <sierrakilo@Sierras-MBP.webpass.net>
2018-07-26 18:24:55 -0700

    basic texts for message area

---
+++ public/text.js
@@ -0,0 +1,13 @@
+// Import it into file like below:
+
+// import {INFO_SHORTCUT_KEYS, INFO_CANCEL_SHORTCUT_KEY} from '../../assets/text/en_US';
+
+// Export each one
+
+// export const MSG_NO_ITEMS = 'There are no items.';
+
+export const WELCOME_MSG = "Welcome to Keepers Market. We are Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+
+export const THANK_YOU_MSG = "Thank you for signing up now you get Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+
+export const SORRY_MSG = "Sorry to see you go please come back later Lorem ipsum dolor sit amet, consectetur adipisicing elit."
