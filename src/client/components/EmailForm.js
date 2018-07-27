4d1b6157a7d2df04da56ab2b40c834896e49b403
Sierra Kilo <sierrakilo@Sierras-MBP.webpass.net>
2018-07-26 22:02:20 -0700

    created onChange, handleSubmitSubscribe / Unsubscribe methods, also gets changeState prop from parent to change state of submitted to change message

--- src/client/components/EmailForm.js
+++ src/client/components/EmailForm.js
@@ -1,14 +1,97 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Button from './Button'
+import axios from 'axios'
 
-const EmailForm = ({props}) => (
-  <div>
-    <form>
-      <input/>
-      {/* Button needs props to display correct label */}
-      <Button/>
-    </form>
-  </div>
-);
+class EmailForm extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      email: '',
+      error: ''
+    }
+    this.onEmailChange = this.onEmailChange.bind(this)
+    this.handleSubmitSubscribe = this.handleSubmitSubscribe.bind(this)
+    this.handleSubmitUnsubscribe = this.handleSubmitUnsubscribe.bind(this)
+  }
+  onEmailChange = (e) => {
+    // Materialize.updateTextFields();
+    const email = e.target.value;
+    this.setState(() => ({ email }))
+  }
+
+  handleSubmitSubscribe = (e) => {
+    e.preventDefault();
+    const email = this.state.email.toLowerCase()  ;
+    this.setState(() => ({
+      email: ''
+    }))
+    axios.post("/api/emails/", {
+      email
+    })
+    .then(function (response) {
+      console.log(response);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+    this.props.changeState()
+  }
+
+  handleSubmitUnsubscribe = (e) => {
+  e.preventDefault();
+  const email = this.state.email.toLowerCase();
+  this.setState(() => ({
+    email: ''
+  }))
+
+  axios.delete("/api/emails/delete/", {
+    data: {email: email}
+  })
+  .then(function (response) {
+    console.log(response);
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
+  this.props.changeState()
+}
+
+  render() {
+    return (
+      <div>
+        <form
+          // onSubmit={this.onSubmit.bind(this)}
+          // onSubmit(e) {
+          //   if(this.props.actionType === 'submit') {
+          //     this.handleSubmitSubscribe(e)
+          //     this.props.changeState()
+          //   } else {
+          //     this.handleSubmitUnsubscribe(e)
+          //     this.props.changeState()
+          //   }
+          // }
+
+
+          onSubmit={(e) => {
+          (this.props.actionType === 'submit') ?
+          this.handleSubmitSubscribe(e)
+          :
+          this.handleSubmitUnsubscribe(e)
+        }}>
+
+
+          {/* input needs props to set perams */}
+          <input
+            value={this.state.email}
+            onChange={this.onEmailChange}/>
+          {/* Button needs props to display correct label */}
+          <Button/>
+        </form>
+      </div>
+    );
+  }
+
+}
 
 export default EmailForm;
