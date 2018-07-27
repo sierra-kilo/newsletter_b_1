cc2eb89b0e6f2d58c13ef863f8fb5552794cd74a
Sierra Kilo <sierrakilo@Sierras-MBP.webpass.net>
2018-07-26 18:26:45 -0700

    Subscribe page renders proper message based on new state of submitted

--- src/client/components/SubscribePage.js
+++ src/client/components/SubscribePage.js
@@ -1,15 +1,41 @@
 import React, { Component } from 'react';
 import EmailForm from './EmailForm'
 import MessageArea from './MessageArea'
+import {WELCOME_MSG, THANK_YOU_MSG} from '../../../public/text';
 
 class SubscribePage extends Component {
+  constructor(props) {
+  super(props)
+  this.state = {
+    submitted: false
+  }
+  this.changeState = this.changeState.bind(this)
+}
+
+changeState() {
+  this.setState({
+    submitted: true //!(this.state.submitted)
+  })
+  setInterval(() => {
+    this.setState({
+      submitted: false
+    })
+  }, 20000)
+}
 
   render() {
     return (
       <div>
         <h3>Subscribe Here Page</h3>
-        <MessageArea/>
-        <EmailForm/>
+        {!(this.state.submitted) ? (
+          <div>
+            <MessageArea message={WELCOME_MSG}/>
+            <EmailForm/>
+            {/* <Subscribe changeState={this.changeState}/> */}
+          </div>
+        ) : (
+          <MessageArea message={THANK_YOU_MSG}/>
+        )}
       </div>
     );
   }
