35418cd259b1b77974f78dc091d5f325c0c809f0
Sierra Kilo <sierrakilo@Sierras-MBP.webpass.net>
2018-07-26 22:23:23 -0700

    redirect to subscribe page after unsubscribe

--- src/client/components/UnsubscribePage.js
+++ src/client/components/UnsubscribePage.js
@@ -2,8 +2,28 @@ import React, { Component } from 'react';
 import EmailForm from './EmailForm'
 import MessageArea from './MessageArea'
 import {SORRY_MSG, COME_BACK_MSG} from '../../../public/text';
+import { Redirect } from 'react-router'
 
 class UnsubscribePage extends Component {
+  constructor(props) {
+  super(props)
+  this.state = {
+    submitted: false
+  }
+  this.changeState = this.changeState.bind(this)
+}
+
+changeState() {
+  this.setState({
+    submitted: true //!(this.state.submitted)
+  })
+  setInterval(() => {
+    this.setState({
+      submitted: false
+    })
+    this.props.history.push('/')
+  }, 20000);
+}
 
   render() {
     return (
