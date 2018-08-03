91e314d60f11b2f4ad911b77e76d930f91774068
Sierra Kilo <sierrakilo@Sierras-MBP.DFMNET01>
2018-08-03 11:38:17 -0700

    added materialize-css folders to style structure and sass-loader and dependecies.  also formatted Header component, not yet final

--- src/client/components/Header.js
+++ src/client/components/Header.js
@@ -1,18 +1,22 @@
 import React, { Component } from 'react';
 
-const imgStyle = {
-  maxHeight: '16.5px'
-}
-
 class Header extends Component {
 
   render() {
     return (
       <div>
-        <h2>
+        <nav>
+          <div className="nav-wrapper">
+            <img
+              className='brand-logo left logo-img responsive-img'
+              src={'../../../public/favicon.ico'}/>
+              <h3 className='header-title right'>Keepers Market</h3>
+          </div>
+        </nav>
+        {/* <h2 className='header-title'>
           <img src={'../../../public/favicon.ico'} style={imgStyle}/>
           Keepers Market
-        </h2>
+        </h2> */}
       </div>
     );
   }
