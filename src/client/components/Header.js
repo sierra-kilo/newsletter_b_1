4036125e824561269bb64f1ac604f6256ca6747b
Sierra Kilo <sierrakilo@Sierras-MBP.webpass.net>
2018-08-04 16:24:56 -0700

    removed path to favicon for logo

--- src/client/components/Header.js
+++ src/client/components/Header.js
@@ -9,7 +9,7 @@ class Header extends Component {
           <div className="nav-wrapper">
             <img
               className='brand-logo left logo-img responsive-img'
-              src={'src/client/images/logo.png'}/>
+              src={'favicon.ico'}/>
               <h3 className='header-title right'>Keepers Market</h3>
           </div>
         </nav>
