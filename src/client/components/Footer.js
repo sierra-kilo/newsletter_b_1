0e7ffab70b4b9abb837204b34fe423fc71a9a306
Sierra Kilo <sierrakilo@Sierras-MacBook-Pro.local>
2018-08-09 13:30:02 -0700

    added new favicon image

--- src/client/components/Footer.js
+++ src/client/components/Footer.js
@@ -3,14 +3,20 @@ import {Link} from 'react-router-dom';
 
 
 const Footer = ({}) => (
-  <div className='center-align page-footer footer'>
-    <Link className="waves-effect waves-teal btn-flat white-text"
-     to={'/'}> Subscribe </Link>
-    <Link className="waves-effect waves-teal btn-flat white-text"
-      to={'/unsubscribe'}> Unsubscribe </Link>
-    <Link className="waves-effect waves-teal btn-flat white-text"
-      to={'/admin'} > Admin </Link>
+  <div>
+    <div className='footer-logo-div'>
+      <img className='footer-logo responsive-img center' src={'favicon.ico'}/>
+    </div>
+    <div className='center-align page-footer footer'>
+      <Link className="waves-effect waves-teal btn-flat white-text"
+       to={'/'}> Subscribe </Link>
+      <Link className="waves-effect waves-teal btn-flat white-text"
+        to={'/unsubscribe'}> Unsubscribe </Link>
+      <Link className="waves-effect waves-teal btn-flat white-text"
+        to={'/admin'} > Admin </Link>
+    </div>
   </div>
+
 );
 
 export default Footer;
