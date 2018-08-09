2f03fc32670131a8b237f4c086ded5bd10bc6bf1
Sierra Kilo <sierrakilo@Sierras-MacBook-Pro.local>
2018-08-09 13:36:46 -0700

    styled new footer with logo above footer bar

--- src/client/components/Footer.js
+++ src/client/components/Footer.js
@@ -3,20 +3,14 @@ import {Link} from 'react-router-dom';
 
 
 const Footer = ({}) => (
-  <div>
-    <div className='footer-logo-div'>
-      <img className='footer-logo responsive-img center' src={'favicon.ico'}/>
-    </div>
-    <div className='center-align page-footer footer'>
-      <Link className="waves-effect waves-teal btn-flat white-text"
-       to={'/'}> Subscribe </Link>
-      <Link className="waves-effect waves-teal btn-flat white-text"
-        to={'/unsubscribe'}> Unsubscribe </Link>
-      <Link className="waves-effect waves-teal btn-flat white-text"
-        to={'/admin'} > Admin </Link>
-    </div>
+  <div className='center-align page-footer footer'>
+    <Link className="waves-effect waves-teal btn-flat white-text"
+     to={'/'}> Subscribe </Link>
+    <Link className="waves-effect waves-teal btn-flat white-text"
+      to={'/unsubscribe'}> Unsubscribe </Link>
+    <Link className="waves-effect waves-teal btn-flat white-text"
+      to={'/admin'} > Admin </Link>
   </div>
-
 );
 
 export default Footer;
