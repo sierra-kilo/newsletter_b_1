03a5338e7834520a4c579c80f45d8d957eb272fd
Shant Kevorkian <shant.kevorkian@gmail.com>
2018-08-19 20:54:01 -0700

    corrected type in highlighted

--- public/text.js
+++ public/text.js
@@ -8,7 +8,7 @@
 
 export const WELCOME_MSG = "Welcome and thanks for visiting! We are a women owned and run resale market specializing in new, vintage and lightly loved epic and one of a kind pieces. You can shop our gems in the Bay Area flea market scene, Indigo Vintage in Berkeley and even through our Instagram. Sign up for our newsletter to find out about upcoming flea appearances, consignment opportunities and exclusive promotions just for you!"
 
-export const THANK_YOU_MSG = "Thank you for signing up for our newsletter updates.  Now you will be kept up to date on our upcoming events, highlited new items, and anything else we are up to!"
+export const THANK_YOU_MSG = "Thank you for signing up for our newsletter updates.  Now you will be kept up to date on our upcoming events, highlighted new items, and anything else we are up to!"
 
 export const SORRY_MSG = "Sorry to see you go please come back later so you wont miss our next great finds!"
 
